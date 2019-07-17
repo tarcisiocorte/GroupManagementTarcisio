359e3f7cc875f0abb26085f4ce450d8b67a66270
tarcisiocorte <tarcisiocorte@gmail.com>
2019-07-16 23:55:59 -0300

    first view

--- src/TCCP.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
+++ src/TCCP.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
@@ -1,15 +1,20 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using TCCP.PlayBall.GroupManagement.Web.Models;
 
 namespace TCCP.PlayBall.GroupManagement.Web.Controllers
 {
     [Route("groups")]
     public class GroupsController: Controller
     {
+        private static List<GroupViewModel> groups = new List<GroupViewModel>
+            {new GroupViewModel() {Id = 1, Name = "Isso é test"}};
+
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
-            return Content("Heloooooow!!!");            
+            return View(groups);
         }
     }
 }
